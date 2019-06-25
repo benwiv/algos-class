513351c25f81db5302b5662f98103f5ceb88d4e8
Ben Waller <benwalleriv@gmail.com>
2019-06-25 08:30:04 -0700

    both pointers solutions included, while and for loop versions

--- test_062119/pointers-2-unique-values.js
+++ test_062119/pointers-2-unique-values.js
@@ -27,35 +27,47 @@
 // console.log(countUniqueValues(test1));
 
 
+
+
+// const countUniqueValues = (sortedArr) => {
+//   let left = 0;
+//   let right= 1;
+//   let count = 0;
+
+//   while (right < sortedArr.length) {
+//     let arr = sortedArr;
+
+//     if (arr[left] === arr[right]) {
+//       right++;
+//     }   
+//     else {
+//       left++;
+//       arr[left] = arr[right];
+//       right++;
+//     }
+//     count = left+1;
+//   }
+//   return count
+// }
+
+
+let test2=[-11,-9,-6,-4,-4,0,1,2,2,3,5,16,111,1777];
 let test1=[-11,-9,-6,-4,-4,0,0,0,0,1,2,2,3,5,16,111,1777];
 
-const countUniqueValues = (sortedArr) => {
-  let left = 0;
-  let right= 1;
-  let count = 0;
-
-  while (right < sortedArr.length) {
-    let arr = sortedArr;
-
-    if (arr[left] === arr[right]) {
-      right++;
-    }   
-    else {
-      left++;
-      arr[left] = arr[right];
-      right++;
-    }
-    count = left+1;
-  }
-  return count
-}
-
-console.log(countUniqueValues(test1));
-
-const countUniqueValues2 = (sortedArr) => {
+const countUniqueValues2 = (arr) => {
   let i = 0;
 
-  for (let j = 1; j < sortedArr.length; j++) {
-    if (sortedArr(i) !== sortedArr(j))
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
   }
+
+  return i+1;
 }
+console.log(countUniqueValues2(test1));
+// console.log(countUniqueValues(test1));
+
+console.log(countUniqueValues2(test2));
+// console.log(countUniqueValues(test2));
